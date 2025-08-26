ba5eff1a7512949f7713122a985af2c748b45dc4
ODajan <osama.string@gmail.com>
2025-08-26 15:01:28 +0300

    changes for arafat

--- Warehousing.UI/src/environments/environment.ts
+++ Warehousing.UI/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: false,
     baseUrl1: 'http://localhost:5036/api',
     resourcesUrl1: 'http://localhost:5036/',
-    baseUrl: 'http://lsvisoin-002-site2.ntempurl.com/api',
-    resourcesUrl: 'http://lsvisoin-002-site2.ntempurl.com/',
+    baseUrl: 'http://lsvisoin-002-site7.ntempurl.com/api',
+    resourcesUrl: 'http://lsvisoin-002-site7.ntempurl.com/',
 };
